d6acde31cab03e56df8521e785c35c2e0fcd777f
jatin-yadav <jatinyadav19oct@gmail.com>
2024-12-30 14:43:07 +0530

    add new icons for Twitter, YouTube, and Document; update Sidebar component title

--- BrainlyFrontend/src/components/Sidebar.tsx
+++ BrainlyFrontend/src/components/Sidebar.tsx
@@ -1,11 +1,17 @@
 import { OpenBookIcon } from "../icons/OpenBookIcon"
+import { TweeterIcon } from "../icons/TweeterIcon"
+import { YoutubeIcon } from "../icons/YoutubeIcon"
 
 const Sidebar = () => {
     return (
         <div className="h-screen w-72 shadow-md">
             <div className="w-full p-4 flex items-center justify-center gap-2 text-2xl font-bold">
+                <h1 className="">♾️ Second Brain</h1>
+            </div>
+            <div className="text-black">
                 <OpenBookIcon />
-                <h1 className="">Second Brain</h1>
+                <TweeterIcon />
+                <YoutubeIcon />
             </div>
         </div>
     )
