220ab894f7d1f521b5063d853ae77ff13823ee6f
jatin-yadav <jatinyadav19oct@gmail.com>
2025-01-02 00:38:12 +0530

    enhance CreateContentModal with submit button and link input; adjust InputField width

--- BrainlyFrontend/src/components/CreateContentModal.tsx
+++ BrainlyFrontend/src/components/CreateContentModal.tsx
@@ -1,4 +1,5 @@
 import { CloseIcon } from "../icons/CloseIcon";
+import Button from "./Button";
 import InputFeild from "./InputFeild";
 
 interface CreateContentModalProps {
@@ -8,12 +9,15 @@ interface CreateContentModalProps {
 const CreateContentModal: React.FunctionComponent<CreateContentModalProps> = ({ open, onClose }) => {
     return (
         <div>{open && <div className="h-screen w-screen bg-gray-600/70 fixed top-0 left-0 z-50 flex justify-center items-center">
-            <div className="bg-white p-4 rounded-lg">
-                <button onClick={onClose} className="">
-                    <CloseIcon />
-                </button>
+            <div className="bg-white p-4 rounded-lg flex flex-col gap-2">
+                <div className="self-end">
+                    <button onClick={onClose} className="">
+                        <CloseIcon />
+                    </button>
+                </div>
                 <InputFeild label="Title" placeholder="Enter title" onChange={() => { }} />
-                <InputFeild label="Title" placeholder="Enter title" onChange={() => { }} />
+                <InputFeild label="Link" placeholder="Enter title" onChange={() => { }} />
+                <Button text="Submit Content" onClick={() => { }} />
             </div>
         </div>}</div>
     )
--- BrainlyFrontend/src/components/InputFeild.tsx
+++ BrainlyFrontend/src/components/InputFeild.tsx
@@ -2,7 +2,7 @@ const InputFeild = ({ onChange, placeholder, label }: { onChange: () => void, pl
     return (
         <div className="flex flex-col">
             <label htmlFor={label}>{label}</label>
-            <input type="text" id={label} name={label} onChange={onChange} placeholder={placeholder} className="px-4 py-2 border rounded my-2 w-80" />
+            <input type="text" id={label} name={label} onChange={onChange} placeholder={placeholder} className="px-4 py-2 border rounded my-2 min-w-96" />
         </div>
     )
 }
