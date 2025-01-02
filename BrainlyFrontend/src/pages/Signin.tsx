deeefe0fab9eda2d2ab32f8ed94a0a4814dc2f21
jatin-yadav <jatinyadav19oct@gmail.com>
2025-01-03 01:05:34 +0530

    implement signin functionality with loading state; update InputField and Button components

--- BrainlyFrontend/src/pages/Signin.tsx
+++ BrainlyFrontend/src/pages/Signin.tsx
@@ -1,17 +1,34 @@
 import InputFeild from '../components/InputFeild'
 import Button from '../components/Button'
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import axios from 'axios';
+import { BACKEND_URL } from '../config';
 
 
 const Signin = () => {
-    const usernameoremailRef = useRef<HTMLInputElement>(null);
+    const [loading, setLoading] = useState(false);
+
+    const usernameRef = useRef<HTMLInputElement>(null);
     const passwordRef = useRef<HTMLInputElement>(null);
+
+    const signin = async () => {
+        setLoading(true);
+        const data = {
+            username: usernameRef.current?.value,
+            password: passwordRef.current?.value
+        }
+
+        const res = await axios.post(`${BACKEND_URL}/api/v1/users/login`, { ...data })
+        alert(`${res?.data?.message}`)
+        setLoading(false);
+    }
+
     return (
         <div className="h-screen w-screen bg-gray-600 flex justify-center items-center">
             <div className="bg-white p-4 rounded-lg flex flex-col gap-2">
-                <InputFeild label="Username/Email" placeholder="Enter username or  email" refrance={usernameoremailRef} />
+                <InputFeild label="Username" placeholder="Enter username" refrance={usernameRef} />
                 <InputFeild label="Password" placeholder="Enter Password" refrance={passwordRef} />
-                <Button text="Signin" onClick={() => { }} />
+                <Button text="Signin" loading={loading} onClick={() => { signin() }} />
             </div>
         </div>
     )
