80ef1a72a5945ff9e8f7309d43778f408ea69cc4
jatin-yadav <jatinyadav19oct@gmail.com>
2025-01-09 11:21:50 +0530

    added delete functionality to Card component and pass contentId from MasonryGrid

--- BrainlyFrontend/src/components/Card.tsx
+++ BrainlyFrontend/src/components/Card.tsx
@@ -1,27 +1,48 @@
 import { TwitterTweetEmbed } from "react-twitter-embed";
 import { DeleteIcon } from "../icons/DeleteIcon";
 import { ShareIcon } from "../icons/ShareIcon";
+import { BACKEND_URL, contentTypes } from "../config";
+import axios from "axios";
 
 interface CardProps {
     link: string;
     title: string;
     type: 'youtube' | 'tweet';
+    contentId?: string
 }
 
-const Card: React.FC<CardProps> = ({ title, link, type }) => {
+const Card: React.FC<CardProps> = ({ title, link, type, contentId }) => {
+    const icon = contentTypes.filter((item) => (item.type === type))[0].icon;
+
+    const deleteContent = async () => {
+        try {
+            const res = await axios.delete(`${BACKEND_URL}/api/v1/content/delete`, {
+                headers: {
+                    "Authorization": localStorage.getItem("token")
+                }, data: { contentId }
+            })
+            alert(`${res?.data?.message}`)
+        } catch (error) {
+            alert(`${error}`)
+        }
+    }
+
     return (
         <div className="p-4 rounded-md bg-white shadow-md border border-gray-200 flex flex-col items-center">
             <div className="w-full flex justify-between">
-                <div className="flex justify-center items-center gap-1"><ShareIcon /> {title}</div>
+                <div className="flex justify-center items-start gap-1"> {icon()} <span className="font-semibold capitalize">{title}</span></div>
                 <div className="flex justify-center items-center gap-2">
                     <a href={link} target="_blank" className="text-gray-500 hover:text-gray-700">
                         <ShareIcon />
                     </a>
-                    <DeleteIcon /></div>
+                    <button onClick={() => { deleteContent() }} className="text-gray-500 hover:text-gray-700">
+                        <DeleteIcon />
+                    </button>
+                </div>
             </div>
             <div className="py-4 w-full">
                 {type === "youtube" && <iframe className="rounded-md w-full" src={link} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>}
-                
+
                 {type === "tweet" && <TwitterTweetEmbed tweetId={link} />}
             </div>
         </div>
--- BrainlyFrontend/src/components/MasonryGrid.tsx
+++ BrainlyFrontend/src/components/MasonryGrid.tsx
@@ -5,19 +5,20 @@ interface MasonryGridProps {
     items: {
         type: 'youtube' | 'tweet',
         title: string,
-        link: string
+        link: string,
+        _id: string
     }[];
 }
 
 const MasonryGrid: React.FC<MasonryGridProps> = ({ items }) => {
     return (
         <div className="columns-1 sm:columns-2 lg:columns-3 gap-4 space-y-4">
-            {items.map(({ title, type, link }, index) => (
+            {items.map(({ title, type, link, _id }, index) => (
                 <div
                     key={index}
                     className="break-inside-avoid shadow rounded-md hover:shadow-lg transition-shadow duration-300"
                 >
-                    <Card link={link} title={title} type={type} />
+                    <Card link={link} title={title} type={type} contentId={_id} />
                 </div>
             ))}
         </div>
