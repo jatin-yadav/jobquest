8283b140cd741d30e625c780c3b0882b6ce1aaa7
jatin-yadav <jatinyadav19oct@gmail.com>
2025-01-20 23:28:32 +0530

    update Home page layout and add new section for questionnaire creation

--- BrainlyFrontend/src/pages/Home.tsx
+++ BrainlyFrontend/src/pages/Home.tsx
@@ -7,13 +7,24 @@ const Home = () => {
             <div className="fixed w-full top-0">
                 <HomeNavigation />
             </div>
-            <div className="h-screen bg-gray-200 pt-24 container">
+            <div className="bg-gray-200 pt-20 container">
                 <div className="flex flex-col justify-center items-center">
                     <h1 className="text-6xl font-bold text-center px-48">Organize Your Digital Life and Unlock Your Creative Potential</h1>
                     <div className="py-6 mt-10">
 
                         <Link to='/signin'>
                             <span className="text-3xl font-semibold bg-yellow-300 px-8 py-2 rounded-xl">Start Building Your Brain </span>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-gray-200 pt-24 container">
+                <div className="flex flex-col justify-center items-center">
+                    <h1 className="text-6xl font-bold text-center px-48">Organize Your Questions and Answers in a smarter way and Revise them in a Creative Way</h1>
+                    <div className="py-6 mt-10">
+
+                        <Link to='/signin'>
+                            <span className="text-3xl font-semibold bg-yellow-300 px-8 py-2 rounded-xl">Start Building Questionnaire </span>
                         </Link>
                     </div>
                 </div>
